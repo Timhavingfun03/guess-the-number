9b080be74dc311315586ea575d3fc6478feb03f3
Tim Torndal <tim.torndal@skola.botkyrka.se>
2020-11-03 22:29:33 +0100

    Guess the number

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Read instructions. And remove this line.
+// Read instructions. And remove this line
+const textInput = document.getElementById('user-input')
+
+textInput.addEventListener('keyup', function (event){
+if (event.key === 'Enter'){
+
+if (textInput.value == 42){
+    alert('congrats that is right')
+    }
+if (textInput.value > 42){
+    alert('That is a little bit to high')
+    }
+if (textInput.value < 42){
+    alert('That is a little bit to low')
+    }
+    textInput.value = ""
+}})
+console.log()
